664aa7b0f1c1247632f28896d97a03c5e94d666a
matsls09 <mats-liver.skandsen@hotmail.com>
2013-10-10 11:59:51 +0200

    Lab 1 og 2 rettet
    Rettet noen feil på lab1 og lab2

---
+++ lab1 - innlevering 10.sept/proglangs.js
@@ -0,0 +1,7 @@
+function proglangs()
+{
+var mylist=document.getElementById("myList").value;
+document.getElementById("favorite").innerHTML=mylist;
+}
+
+
---
+++ lab2 - innlevering 19.sept/proglangsv2.js
@@ -0,0 +1,13 @@
+function printToOutputBox(selectobj){
+	var e_proglangs = document.getElementById("proglangs");
+	var e_outputbox = document.getElementById("outputbox");
+	e_outputbox.innerHTML = "Du har selektert programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text;
+ }
+
+
+function myFunction()
+{
+alert("Ops! Denne siden er under oppbygging!");
+}
+
+
